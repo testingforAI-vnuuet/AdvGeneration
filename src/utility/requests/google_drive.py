db2961910f85c35361893d22ef9b48145662fa82
dominhkha <17020827@vnu.edu.vn>
2021-04-13 13:47:55 +0700

    [utility] adding google_drive upload files

---
+++ src/utility/requests/google_drive.py
@@ -0,0 +1,34 @@
+import os
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+# upload files or folder to drive
+class google_drive:
+
+    def __init__(self):
+        self.folder_path = None
+        self.file_paths = None
+
+    def upload_files(self, folder_path=None, file_paths=None):
+        gauth = GoogleAuth()
+        drive = GoogleDrive(gauth)
+        self.folder_path = folder_path
+        self.file_paths = file_paths
+
+        if self.folder_path is not None:
+            self.file_paths = [os.path.join(self.folder_path, f) for f in os.listdir(self.folder_path) if
+                               os.path.isfile(os.path.join(self.folder_path, f))]
+        elif self.file_paths is None:
+            return
+        for file_path in self.file_paths:
+            gfile = drive.CreateFile({'parents': [{'id': '1VO1ELKNlBGD9gNL6WQv7YDaLcIn9LLkX'}], 'title': os.path.basename(file_path)})
+            gfile.SetContentFile(file_path)
+            gfile.Upload()  # Upload the file.
+
+
+if __name__ == '__main__':
+    folder_name = '../../attacker/saved_models'
+    drive = google_drive()
+    drive.upload_files(folder_name)
