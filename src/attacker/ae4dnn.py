5845db760a82fbdd92a22fb6b248d9eb221fee16
dominhkha <17020827@vnu.edu.vn>
2021-05-27 18:23:57 +0700

    [ae4dnn] refactor ae4dnn attacker for thesis

--- src/attacker/ae4dnn.py
+++ src/attacker/ae4dnn.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 import os
-import threading
 import time
+from collections import defaultdict
 
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 
@@ -11,7 +11,6 @@ from attacker.losses import *
 from attacker.mnist_utils import reject_outliers, L0, L2
 from data_preprocessing.mnist import MnistPreprocessing
 from utility.statistics import *
-from utility.filters.filter_advs import *
 
 logger = MyLogger.getLog()
 
@@ -38,16 +37,20 @@ class AE4DNN:
         self.weight = weight
         self.num_images = num_images
 
-        self.origin_images, self.origin_labels = filter_by_label(self.origin_label, self.trainX, self.trainY)
+        # self.origin_images, self.origin_labels = filter_by_label(self.origin_label, self.trainX, self.trainY)
 
-        self.origin_images = np.array(self.origin_images[:self.num_images])
-        self.origin_labels = np.array(self.origin_labels[:self.num_images])
+        self.origin_images = np.array(self.trainX[:self.num_images])
+        self.origin_labels = np.array(self.trainY[:self.num_images])
 
-        self.target_label = label_ranking(self.origin_images, self.classifier)[-1 * self.target_position]
+        if self.origin_label is None:
+            self.origin_label = 'all'
+            self.target_label = DEFAULT_TARGET
+        else:
+            self.target_label = label_ranking(self.origin_images, self.classifier)[-1 * self.target_position]
         self.target_vector = tf.keras.utils.to_categorical(self.target_label, MNIST_NUM_CLASSES, dtype='float32')
 
         self.autoencoder = None
-        self.file_shared_name = self.method_name + '_' + classifier_name + f'_{origin_label}_{self.target_label}' + 'weight=' + str(
+        self.file_shared_name = self.method_name + '_' + classifier_name + f'_{self.origin_label}_{self.target_label}' + 'weight=' + str(
             self.weight).replace('.', ',') + '_' + str(self.num_images)
 
         self.autoencoder_file_name = self.file_shared_name + 'autoencoder' + '.h5'
@@ -60,6 +63,9 @@ class AE4DNN:
         self.origin_adv_result_file_path = self.file_shared_name + '_origin_adv_result' + '.npy'
         self.num_avg_redundant_pixels = 0
         self.smooth_adv = None
+
+        self.transfer_rate_results = defaultdict()
+        self.hidden_models_name = None
         # self.target = target
 
     # self.target_label_onehot = keras.utils.to_categorical(target, nClasses, dtype='float32')
@@ -102,44 +108,60 @@ class AE4DNN:
                                                                              self.generated_candidates,
                                                                              self.target_label,
                                                                              cnn_model=self.classifier)
-        # self.adv_result, self.num_avg_redundant_pixels = restore_redundant_mnist_pixels(self.classifier,
-        #                                                                                 self.adv_result,
-        #                                                                                 self.origin_adv_result,
-        #                                                                                 self.target_label)
+
         # np.save(os.path.join(SAVED_NPY_PATH, self.method_name, self.adv_result_file_path), self.adv_result)
         # np.save(os.path.join(SAVED_NPY_PATH, self.method_name, self.origin_adv_result_file_path),
         #         self.origin_adv_result)
         # self.smooth_adv = smooth_adv_border_V2(self.classifier, self.adv_result[:5], self.origin_adv_result[:5], self.target_label)
         self.end_time = time.time()
 
+    def black_box_attack(self, hidden_models_name):
+
+        transfer_rate_results = defaultdict()
+        self.hidden_models_name = hidden_models_name
+        for hidden_model_name in hidden_models_name:
+            hidden_model = tf.keras.models.load_model(PRETRAIN_CLASSIFIER_PATH + '/' + hidden_model_name + '.h5')
+            advs, _, _, _ = filter_candidate_adv(self.origin_adv_result,
+                                                 self.adv_result,
+                                                 self.target_label,
+                                                 cnn_model=hidden_model)
+            transfer_rate_results[hidden_model_name] = advs.shape[0] / float(self.adv_result.shape[0])
+
     def export_result(self):
         result = '<=========='
-        str_smooth_adv = list(map(str, self.smooth_adv))
-        result += '\n' + '\n'.join(str_smooth_adv)
-        # result += '\norigin=' + str(self.origin_label) + ',target=' + str(self.target_label) + '\n'
-        # result += '\n\tweight=' + str(self.weight)
-        # result += '\n\t#adv=' + str(self.adv_result.shape[0])
-        # result += '\n\t#optimal_epoch=' + str(self.optimal_epoch)
+        # str_smooth_adv = list(map(str, self.smooth_adv))
+        # result += '\n' + '\n'.join(str_smooth_adv)
+        result += '\norigin=' + str(self.origin_label) + ',target=' + str(self.target_label) + '\n'
+        result += '\n\tweight=' + str(self.weight)
+        result += '\n\t#adv=' + str(self.adv_result.shape[0])
+        result += '\n\t#optimal_epoch=' + str(self.optimal_epoch)
         # result += '\n\t#avg_redundant_pixels=' + str(self.num_avg_redundant_pixels)
-        # l0 = np.array([L0(gen, test) for gen, test in zip(self.adv_result, self.origin_adv_result)])
-        # l0 = reject_outliers(l0)
-        #
-        # if l0.shape[0] != 0:
-        #     result += '\n\tl0=' + str(min(l0)) + '/' + str(max(l0)) + '/' + str(np.average(l0))
-        # else:
-        #     result += '\n\tl0=None'
-        #
-        # l2 = np.array([L2(gen, test) for gen, test in zip(self.adv_result, self.origin_adv_result)])
-        # l2 = reject_outliers(l2)
-        #
-        # if l2.shape[0] != 0:
-        #     result += '\n\tl2=' + str(round(min(l2), 2)) + '/' + str(round(max(l2), 2)) + '/' + str(
-        #         round(np.average(l2), 2))
-        # else:
-        #     result += '\n\tl2=None'
-        # result += '\n\ttime=' + str(self.end_time - self.start_time) + ' s'
-        # result += '\n==========>\n'
+        l0 = np.array([L0(gen, test) for gen, test in zip(self.adv_result, self.origin_adv_result)])
+        l0 = reject_outliers(l0)
 
+        if l0.shape[0] != 0:
+            result += '\n\tl0=' + str(min(l0)) + '/' + str(max(l0)) + '/' + str(np.average(l0))
+        else:
+            result += '\n\tl0=None'
+
+        l2 = np.array([L2(gen, test) for gen, test in zip(self.adv_result, self.origin_adv_result)])
+        l2 = reject_outliers(l2)
+
+        if l2.shape[0] != 0:
+            result += '\n\tl2=' + str(round(min(l2), 2)) + '/' + str(round(max(l2), 2)) + '/' + str(
+                round(np.average(l2), 2))
+        else:
+            result += '\n\tl2=None'
+        result += '\n\ttime=' + str(self.end_time - self.start_time) + ' s'
+        result += '\n\ttransfer rate: '
+
+        result += '\n==========>\n'
+        transfer_txt = ''
+        if self.hidden_models_name is not None:
+            transfer_txt = '\ntransferable rate: '
+            for item in self.transfer_rate_results:
+                transfer_txt += f'{item} ({self.transfer_rate_results[item]} '
+        result += transfer_txt
         f = open(os.path.join('result', self.method_name, self.file_shared_name + '.txt', ), 'w')
         f.write(result)
         f.close()
@@ -207,107 +229,15 @@ class AE4DNN:
         plot_images(origin_image_worst_l0, origin_image_best_l0, gen_image_worst_l0, gen_image_best_l0, l2_l0_worst,
                     l2_l0_best, l0_worst, l0_best, path_l0, self.classifier, worst_l0_index, best_l0_index)
 
-    # def get_architecture(self, input_shape=(MNIST_IMG_ROWS, MNIST_IMG_COLS, MNIST_IMG_CHL)):
-    #     input_img = keras.layers.Input(shape=input_shape)
-    #     x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-    #     x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    #     x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    #     encoded = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    #
-    #     x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
-    #     x = keras.layers.UpSampling2D((2, 2))(x)
-    #     x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    #     x = keras.layers.UpSampling2D((2, 2))(x)
-    #     decoded = keras.layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-    #     self.auto_encoder = keras.models.Model(input_img, decoded)
-    #     self.auto_encoder.summary()
-
-    # def compile(self, classifier, loss):
-    #     if not isinstance(self.auto_encoder, keras.models.Model):
-    #         self.get_architecture()
-    #
-    #     adam = keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-    #     self.auto_encoder.compile(optimizer=adam,
-    #                               loss=loss(classifier, self.target_label_onehot, weight=0.005, alpha=1.5))
-    #     self.is_compiled = True
-    #
-    # def fit(self, epochs, batch_size):
-    #     """
-    #     :param epochs: epochs
-    #     :param batch_size: batch size
-    #     :return: null
-    #     """
-    #     if not self.is_compiled:
-    #         raise ValueError("Compile the autoencoder first")
-    #     self.auto_encoder.train(self.get_seed_images(), self.get_seed_images(), epochs=epochs, batch_size=batch_size)
-    #     self.plot(self.auto_encoder.history)
-
-    # def plot(self, history):
-    #     plt.plot(history.history['loss'])
-    #     plt.title('Loss')
-    #     plt.ylabel('loss')
-    #     plt.xlabel('epoch')
-    #     # plt.legend(['train', 'test'], loc='upper left')
-    #     plt.show()
-
-
-def run_thread(classifier_name, trainX, trainY):
-    cnn_model = tf.keras.models.load_model(PRETRAIN_CLASSIFIER_PATH + '/' + classifier_name + '.h5')
-    result_txt = classifier_name + '\n'
-
-    origin_label = 9
-    target_position = 2
-
-    for weight_index in range(1, 11):
-        weight_value = weight_index * 0.1
-        attacker = AE4DNN(trainX=trainX, trainY=trainY, origin_label=origin_label,
-                          target_position=target_position, classifier=cnn_model,
-                          classifier_name=classifier_name, weight=weight_value)
-        attacker.autoencoder_attack(loss=AE_LOSSES.cross_entropy_loss)
-        _ = attacker.export_result()
-        del attacker
-        # res_txt, _ = attacker.export_result()
-        # attacker.save_images()
-        # result_txt += res_txt
-
-    # f = open('./result/ae4dnn/' + classifier_name + str(origin_label) + '.txt', 'w')
-    # f.write(result_txt)
-    # f.close()
-
-
-class MyThread(threading.Thread):
-    def __init__(self, classifier_name, trainX, trainY):
-        super(MyThread, self).__init__()
-        self.classifier_name = classifier_name
-        self.trainX = trainX
-        self.trainY = trainY
-
-    def run(self):
-        run_thread(self.classifier_name, self.trainX, self.trainY)
-
 
 if __name__ == '__main__':
     START_SEED = 0
     END_SEED = 1000
-    TARGET = 7
+    DEFAULT_TARGET = 7
+    DEFAULT_EPSILON = 0.005
     ATTACKED_CNN_MODEL = CLASSIFIER_PATH + '/pretrained_mnist_cnn1.h5'
     AE_MODEL = CLASSIFIER_PATH + '/autoencoder_rerank_mnist.h5'
     AE_LOSS = AE_LOSSES.re_rank_loss
-
-    # load dataset
-    # (trainX, trainY), (testX, testY) = mnist.load_data()
-    # pre_mnist = MnistPreprocessing(trainX, trainY, testX, testY, START_SEED, END_SEED, TARGET)
-    # trainX, trainY, testX, testY = pre_mnist.preprocess_data()
-    # countSamples(probability_vector=trainY, n_class=MNIST_NUM_CLASSES)
-    #
-    # # train an autoencoder
-    # classifier = keras.models.load_model(ATTACKED_CNN_MODEL)
-    # auto_encoder = Auto_encoder_rerank(train_data=trainX, target=TARGET, nClasses=MNIST_NUM_CLASSES)
-    # auto_encoder.compile(classifier=classifier, loss=AE_LOSS)
-    # auto_encoder.fit(epochs=300, batch_size=512)
-    #
-    # logger.debug("Export the trained auto-encoder to file")
-    # auto_encoder.auto_encoder.save(AE_MODEL)
 
     (trainX, trainY), (testX, testY) = keras.datasets.mnist.load_data()
 
@@ -318,17 +248,11 @@ if __name__ == '__main__':
                                                             rows=MNIST_IMG_ROWS, cols=MNIST_IMG_COLS,
                                                             chl=MNIST_IMG_CHL)
 
-    thread1 = MyThread(pretrained_model_name[0], trainX, trainY)
-    # thread2 = MyThread(pretrained_model_name[1], trainX, trainY)
-    # thread3 = MyThread(pretrained_model_name[2], trainX, trainY)
-    # thread4 = MyThread(pretrained_model_name[3], trainX, trainY)
+    classifier = keras.models.load_model(ATTACKED_CNN_MODEL)
 
-    thread1.start()
-    # thread2.start()
-    # thread3.start()
-    # thread4.start()
+    ae4dnn_attack = AE4DNN(trainX=trainX, trainY=trainY, origin_label=None, target_position=None, classifier=classifier,
+                           weight=DEFAULT_EPSILON, classifier_name='targetmodel', num_images=1000)
 
-    thread1.join()
-    # thread2.join()
-    # thread3.join()
-    # thread4.join()
+    ae4dnn_attack.autoencoder_attack(loss=AE_LOSSES.border_loss)
+    ae4dnn_attack.black_box_attack(pretrained_model_name)
+    ae4dnn_attack.export_result()
